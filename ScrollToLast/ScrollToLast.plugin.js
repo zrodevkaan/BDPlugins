95491688f91fc0a435aeb3debd0a10356ba71c11
ImAFrogOwO <dev.zero06@gmail.com>
2023-09-02 21:47:29 -0500

    Added ScrollToLast.
    thanks square :D

---
+++ ScrollToLast/ScrollToLast.plugin.js
@@ -0,0 +1,42 @@
+/**
+ * @name ScrollToLast
+ * @author square,imafrogowo
+ * @description Automatically scrolls to the latest messages of any channel you select!
+ */
+
+const {
+    findModuleByProps,
+    Patcher,
+    Webpack: { getModule },
+  } = BdApi;
+  
+  class ScrollToLatest {
+    constructor() {
+      this.Name = ScrollToLatest.name;
+      this.Keybinds = findModuleByProps("MARK_CHANNEL_READ");
+      this.FluxDispatch = getModule(
+        (e) => e.dispatch && !e.emitter && !e.commands
+      );
+  
+      this.onSwitch = this.onSwitch.bind(this);
+    }
+  
+    onSwitch(Gullible) {
+      if (Gullible != undefined) {
+        this.Keybinds.MARK_CHANNEL_READ.action({ target: Gullible });
+      }
+    }
+  
+    start() {
+      this.FluxDispatch.subscribe("CHANNEL_SELECT", this.onSwitch);
+      this.FluxDispatch.subscribe("GUILD_SELECT", this.onSwitch);
+    }
+  
+    stop() {
+      this.FluxDispatch.unsubscribe("CHANNEL_SELECT", this.onSwitch);
+      this.FluxDispatch.unsubscribe("GUILD_SELECT", this.onSwitch);
+    }
+  }
+  
+  module.exports = ScrollToLatest;
+  
