3095c040370c90f03f1dff3b353fba50f92b9bba
ImAFrogOwO <dev.zero06@gmail.com>
2023-10-18 17:49:57 -0500

    Update BiggerAvatars.plugin.js

--- Plugins/BiggerAvatars/BiggerAvatars.plugin.js
+++ Plugins/BiggerAvatars/BiggerAvatars.plugin.js
@@ -1,16 +1,19 @@
 /**
  * @name BiggerAvatars
  * @author imafrogowo
- * @version 1.0.1
+ * @version 1.0.2
  * @description Bigger Avatars
  */
 
-const { Patcher, Webpack, DOM } = BdApi
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
+
+const { Patcher, Webpack, DOM } = BdApi;
 
 class BiggerAvatars {
     constructor() {
         this.name = BiggerAvatars.name
-        this.version = '1.0.1'
+        this.version = '1.0.2'
         this.githubOwner = "ImAFrogOwO"
         this.css = `
                 .image-div {
@@ -70,87 +73,161 @@ class BiggerAvatars {
         }
     }
 
-    handleMouseOver(event) {
-        const imgElement = event.target;
-        if (
-            imgElement.className.includes("avatarDecoration") ||
-            imgElement.currentSrc.includes("decoration")
-        )
-            imgElement.style.pointerEvents = "none";
-        if (imgElement.currentSrc.includes("avatar")) {
-            const src = imgElement.currentSrc;
-            const newSize = 1280;
+  getModuleWithKey(filter) {
+    let target; let id; let key;
 
-            const modifiedSrc = src.replace(/size=\d+/, `size=${newSize}`);
+    Webpack.getModule(
+      (e, m, i) => filter(e, m, i) && (target = m) && (id = i) && true,
+      { searchExports: true },
+    );
 
-            const imageDiv = document.createElement("div");
-            imageDiv.classList.add("image-div");
+    for (const k in target.exports) {
+      if (filter(target.exports[k], target, id)) {
+        key = k;
+        break;
+      }
+    }
 
-            const img = new Image();
-            img.src = modifiedSrc;
+    return [target.exports, key];
+  }
 
-            imageDiv.appendChild(img);
+  handleMouseOver(event) {
+    const imgElement = event.target;
+    /* if (
+      imgElement.className.includes("avatarDecoration") ||
+      imgElement.currentSrc.includes("decoration")
+    )
+      imgElement.style.pointerEvents = "none";
+    else imgElement.style.pointerEvents = "all";*/
 
-            const rect = imgElement.getBoundingClientRect();
-            let left = rect.left + window.scrollX;
-            let top = rect.bottom + window.scrollY;
+    if (
+      imgElement.currentSrc.includes('avatars') ||
+      imgElement.src.includes('avatars')
+    ) {
+      // console.log("imgElement:", imgElement);
+      const src = imgElement.currentSrc;
+      const newSize = 1280;
 
-            if (top + 300 > window.innerHeight) {
-                top = window.innerHeight - 320;
-            }
+      const modifiedSrc = src.replace(/size=\d+/, `size=${newSize}`);
 
-            if (left + 300 > window.innerWidth) {
-                left = window.innerWidth - 320;
-            }
-            imageDiv.style.left = `${left}px`;
-            imageDiv.style.top = `${top}px`;
+      const imageDiv = document.createElement('div');
+      imageDiv.classList.add('image-div');
 
-            img.style.width = "300px";
-            img.style.height = "300px";
-            img.style.borderRadius = "20px";
-            document.body.appendChild(imageDiv);
+      const img = new Image();
+      img.src = modifiedSrc;
 
-            imgElement.addEventListener("mouseleave", () => {
-                if (document.body.contains(imageDiv)) {
-                    document.body.removeChild(imageDiv);
-                }
-            });
+      imageDiv.appendChild(img);
+
+      const rect = imgElement.getBoundingClientRect();
+      let left = rect.left + window.scrollX;
+      let top = rect.bottom + window.scrollY;
+
+      if (top + 300 > window.innerHeight) {
+        top = window.innerHeight - 320;
+      }
+
+      if (left + 300 > window.innerWidth) {
+        left = window.innerWidth - 320;
+      }
+      imageDiv.style.left = `${left}px`;
+      imageDiv.style.top = `${top}px`;
+
+      img.style.width = '300px';
+      img.style.height = '300px';
+      img.style.borderRadius = '20px';
+      document.body.appendChild(imageDiv);
+
+      imgElement.addEventListener('mouseleave', () => {
+        if (document.body.contains(imageDiv)) {
+          document.body.removeChild(imageDiv);
         }
+      });
     }
-    GetElementsBySrc(findSrc) {
-        // This is still lazy as hell.
-        const TerribleElements = document.querySelectorAll(
-            `img[src*="${findSrc}"]`
+  }
+  GetElementsBySrc(findSrc) {
+    // This is still lazy as hell.
+    const TerribleElements = document.querySelectorAll(
+      `img[src*="${findSrc}"]`,
+    );
+
+    return TerribleElements;
+  }
+  start() {
+    const getAvatarURL =
+      Webpack.getStore('UserStore').getCurrentUser().constructor.prototype;
+    const memberListUser = this.getModuleWithKey(
+      BdApi.Webpack.Filters.byStrings('avatarDecorationSrc', 'roleName', 'user'),
+    );
+    const friendsList = this.getModuleWithKey(
+      BdApi.Webpack.Filters.byStrings(
+        'statusBackdropColor',
+        'typingIndicatorRef',
+        'src',
+      ),
+    );
+
+    this.ChatListPatch = Patcher.after(
+      'BiggerAvatars',
+      friendsList[0],
+      'Avatar',
+      (a, b, c, d, e) => {
+        const matchingElements = this.GetElementsBySrc(c.props.src);
+        matchingElements.forEach((imgElement) => {
+          if (!imgElement.hasEventListener) {
+            imgElement.hasEventListener = true;
+            imgElement.addEventListener('mouseover', this.handleMouseOver);
+          }
+        });
+      },
+    );
+
+    this.MemberListPatch = Patcher.after(
+      'BiggerAvatars',
+      memberListUser[0],
+      'Z',
+      (a, b, c, d, e) => {
+        const matchingElement = this.GetElementsBySrc(
+          c.props.avatar.props.children[0].props.src,
         );
+        if (matchingElement[0]) {
+          matchingElement[0].style.pointerEvents = 'all'; // Why does this have to be here and not the actual mouseover event.
+          matchingElement[0].addEventListener(
+            'mouseover',
+            this.handleMouseOver,
+          );
+        }
+      },
+    );
 
-        return TerribleElements;
-    }
-    start() {
-        const getAvatarURL =
-            Webpack.getStore("UserStore").getCurrentUser().constructor
-                .prototype;
+    this.AvatarPatch = Patcher.after(
+      'BiggerAvatars',
+      getAvatarURL,
+      'getAvatarURL',
+      (a, b, c, d, e) => {
+        const matchingElements = this.GetElementsBySrc(c);
+        matchingElements.forEach((imgElement) => {
+          imgElement.style.pointerEvents = 'all';
+          if (!imgElement.hasEventListener) {
+            imgElement.hasEventListener = true;
+            imgElement.addEventListener('mouseover', this.handleMouseOver);
+          }
+        });
+      },
+    );
 
-        Patcher.after(
-            "BiggerAvatars",
-            getAvatarURL,
-            "getAvatarURL",
-            (a, b, c, d, e) => {
-                const matchingElements = this.GetElementsBySrc(c);
-                matchingElements.forEach((imgElement) => {
-                    if (!imgElement.hasEventListener) {
-                        imgElement.hasEventListener = true;
-                        imgElement.addEventListener("mouseover", this.handleMouseOver);
-                    }
-                });
-            }
-        );
+    DOM.addStyle('BiggerImagesCSS', this.css);
+  }
 
-        DOM.addStyle("BiggerImagesCSS", this.css);
-    }
-
-    stop() {
-        DOM.removeStyle("BiggerImagesCSS");
-    }
+  stop() {
+    this.ChatListPatch();
+    this.MemberListPatch();
+    this.AvatarPatch();
+    document.querySelectorAll('.image-div').forEach((img) => {
+      img.remove();
+      img.removeEventListener('mouseover', this.handleMouseOver);
+    });
+    DOM.removeStyle('BiggerImagesCSS');
+  }
 }
 
 module.exports = BiggerAvatars;
