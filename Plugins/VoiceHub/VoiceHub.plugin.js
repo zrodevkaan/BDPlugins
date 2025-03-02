dc239de5c22ec89f921db70f5f32dea3d7b13857
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2025-03-02 14:24:32 -0600

    dropdowns and stuff

--- Plugins/VoiceHub/VoiceHub.plugin.js
+++ Plugins/VoiceHub/VoiceHub.plugin.js
@@ -1,11 +1,11 @@
 /**
  * @name VoiceHub
  * @author Kaan
- * @version 1.0.3
+ * @version 1.0.4
  * @description Wanna know what people are in VCs? Here ya go.
  */
 
-const { Patcher, Webpack, React, DOM } = new BdApi('VoiceHub');
+const { Patcher, Webpack, React, DOM, Data } = new BdApi('VoiceHub');
 const Module = Webpack.getBySource('ConnectedPrivateChannelsList');
 
 const [VoiceIcon, ModalRoot, openModal, SearchIcon, VideoIcon, LiveStream] = BdApi.Webpack.getBulk(
@@ -15,8 +15,52 @@ const [VoiceIcon, ModalRoot, openModal, SearchIcon, VideoIcon, LiveStream] = BdA
     { filter: BdApi.Webpack.Filters.byStrings('"M15.62 17.03a9 9 0 1 1 1.41-1.41l4.68 4.67a1 1 0 0 1-1.42 1.42l-4.67-4.68ZM17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z'), searchExports: true },
     { filter: BdApi.Webpack.Filters.byStrings('"M4 4a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h11a3 3 0 0 0 3-3v-2.12a1 1 0'), searchExports: true },
     { filter: BdApi.Webpack.Filters.byStrings('dI3q4u'), searchExports: true }
-  );
-  
+);
+
+const Eye = ({width, height}) => React.createElement('svg', {
+    viewbox: '0 0 1200 1200',
+    width: width ?? '24px',
+    height: height ?? '24px',
+    color: 'color-mix(in oklab, hsl(0 0% 100%/1) 100%, #000 0%)'
+}, React.createElement('path', {
+    fill: 'color-mix(in oklab, hsl(0 0% 100%/1) 100%, #000 0%)',
+    d: 'M12 5C5.648 5 1 12 1 12C1 12 5.648 19 12 19C18.352 19 23 12 23 12C23 12 18.352 5 12 5ZM12 16C9.791 16 8 14.21 8 12C8 9.79 9.791 8 12 8C14.209 8 16 9.79 16 12C16 14.21 14.209 16 12 16Z M12 14C13.1046 14 14 13.1046 14 12C14 10.8954 13.1046 10 12 10C10.8954 10 10 10.8954 10 12C10 13.1046 10.8954 14 12 14Z'
+}))
+const EyeClose = ({width, height}) => /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    viewBox: "0 89.9801 1200 1020",
+    color: 'color-mix(in oklab, hsl(0 0% 100%/1) 100%, #000 0%)',
+    width: width ?? '24px', 
+    height: height ?? '24px',
+    children: /*#__PURE__*/React.createElement("path", {
+        d: "M669.727,273.516c-22.891-2.476-46.15-3.895-69.727-4.248c-103.025,0.457-209.823,25.517-310.913,73.536  c-75.058,37.122-148.173,89.529-211.67,154.174C46.232,529.978,6.431,577.76,0,628.74c0.76,44.162,48.153,98.67,77.417,131.764  c59.543,62.106,130.754,113.013,211.67,154.174c2.75,1.335,5.51,2.654,8.276,3.955l-75.072,131.102l102.005,60.286l551.416-960.033  l-98.186-60.008L669.727,273.516z M902.563,338.995l-74.927,129.857c34.47,44.782,54.932,100.006,54.932,159.888  c0,149.257-126.522,270.264-282.642,270.264c-6.749,0-13.29-0.728-19.922-1.172l-49.585,85.84c22.868,2.449,45.99,4.233,69.58,4.541  c103.123-0.463,209.861-25.812,310.84-73.535c75.058-37.122,148.246-89.529,211.743-154.174  c31.186-32.999,70.985-80.782,77.417-131.764c-0.76-44.161-48.153-98.669-77.417-131.763  c-59.543-62.106-130.827-113.013-211.743-154.175C908.108,341.478,905.312,340.287,902.563,338.995L902.563,338.995z   M599.927,358.478c6.846,0,13.638,0.274,20.361,0.732l-58.081,100.561c-81.514,16.526-142.676,85.88-142.676,168.897  c0,20.854,3.841,40.819,10.913,59.325c0.008,0.021-0.008,0.053,0,0.074l-58.228,100.854  c-34.551-44.823-54.932-100.229-54.932-160.182C317.285,479.484,443.808,358.477,599.927,358.478L599.927,358.478z M768.896,570.513  L638.013,797.271c81.076-16.837,141.797-85.875,141.797-168.603C779.81,608.194,775.724,588.729,768.896,570.513L768.896,570.513z",
+        fill: "color-mix(in oklab, hsl(0 0% 100%/1) 100%, #000 0%)"
+    })
+});
+
+const DataStore = new Proxy(
+    {},
+    {
+        get: (_, key) => {
+            /*if (key === 'settings') {
+                const savedSettings = Data.load(key) || {};
+                return baseConfig.defaultConfig.reduce((acc, setting) => {
+                    acc[setting.id] = savedSettings[setting.id] ?? setting.value;
+                    return acc;
+                }, {});
+            }*/
+            return Data.load(key);
+        },
+        set: (_, key, value) => {
+            Data.save(key, value);
+            return true;
+        },
+        deleteProperty: (_, key) => {
+            Data.delete(key);
+            return true;
+        },
+    }
+);
 
 const InteractiveModule = Webpack.getByKeys('interactive', 'muted', 'selected');
 const InteractiveAbove = Webpack.getByKeys('channel', 'interactiveSystemDM', 'interactiveSelected');
@@ -171,6 +215,8 @@ const VoiceChannelList = () => {
     const [filterType, setFilterType] = React.useState('all'); // deprecated/useless idea. dont mind this.
     const guilds = GuildStore.getGuilds();
 
+    const [dropped, setDropped] = React.useState(DataStore?.hiddenGuilds || []);
+
     const searchLower = searchQuery.toLowerCase();
 
     const filteredGuilds = Object.values(guilds).filter(guild => {
@@ -206,10 +252,21 @@ const VoiceChannelList = () => {
             });
     });
 
+    const toggleDropped = (guildId) => {
+        const yue = {
+            ...dropped,
+            [guildId]: !dropped[guildId]
+        }
+
+        setDropped(yue)
+
+        DataStore.hiddenGuilds = yue
+    };
+
     return React.createElement('div', {
         style: {
             padding: '20px',
-            maxHeight: '600px',
+            height: '600px',
             display: 'flex',
             flexDirection: 'column',
         }
@@ -257,9 +314,9 @@ const VoiceChannelList = () => {
                 key: guild.id,
                 style: {
                     marginBottom: '24px'
-                }
+                },
             }, [
-                React.createElement('div', { style: { display: 'flex', gap: '10px' } },
+                React.createElement('div', { style: { display: 'flex', gap: '10px' }, onClick: () => toggleDropped(guild.id) },
                     React.createElement('img', {
                         src: guild?.icon ? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.webp?size=1280&quality=lossless` : `https://cdn.discordapp.com/embed/avatars/${getAvatar(guild.id)}.png`,
                         style: {
@@ -274,18 +331,21 @@ const VoiceChannelList = () => {
                             color: 'var(--header-primary)',
                             fontSize: '16px',
                             fontWeight: '600',
-                            letterSpacing: '0.5px'
+                            letterSpacing: '0.5px',
+                            gap: '10px',
+                            display: 'flex',
+                            alignItems: 'center',
                         }
-                    }, guild.name),
+                    }, [guild.name, React.createElement(dropped[guild.id] ? EyeClose : Eye, {width: '24px', height: '24px'})]),
                 ),
-                ...activeChannels.map(channel =>
+                !dropped[guild.id] ? activeChannels.map(channel =>
                     React.createElement(CustomVoiceChannel, {
                         key: channel.id,
                         channel: channel,
                         guild: guild,
                         voiceStates: voiceStates
                     })
-                )
+                ) : React.createElement('div')
             ]);
         }))
     ]);
