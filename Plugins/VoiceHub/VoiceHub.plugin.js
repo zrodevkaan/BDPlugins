40920f4a4635ef0aee83c5df007f5e40b7c00dff
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2025-06-29 01:45:38 -0500

    bwah

--- Plugins/VoiceHub/VoiceHub.plugin.js
+++ Plugins/VoiceHub/VoiceHub.plugin.js
@@ -30,7 +30,7 @@ __export(index_exports, {
 });
 module.exports = __toCommonJS(index_exports);
 var { Patcher, Webpack, React, DOM, Data } = new BdApi("VoiceHub");
-var Module = Webpack.getBySource("ConnectedPrivateChannelsList");
+var Module = Webpack.getBySource(".Z.CONTACTS_LIST");
 var [VoiceIcon, ModalRoot, openModal, SearchIcon, VideoIcon, LiveStream] = BdApi.Webpack.getBulk(
   { filter: BdApi.Webpack.Filters.byStrings('"M15.16 16.51c-.57.28-1.16-.2-1.16-.83v-.14c0-.43.28-.8.63-1.02a3 3 0 0 0 0-5.04c-.35-.23-.63-.6-.63-1.02v-.14c0-.63.59-1.1 1.16-.83a5 5 0 0 1 0 9.02Z'), searchExports: true },
   { filter: BdApi.Webpack.Filters.byStrings('.ImpressionTypes.MODAL,"aria-labelledby":'), searchExports: true },
@@ -42,7 +42,7 @@ var [VoiceIcon, ModalRoot, openModal, SearchIcon, VideoIcon, LiveStream] = BdApi
 var Eye = ({ width, height }) => /* @__PURE__ */ BdApi.React.createElement(
   "svg",
   {
-    viewBox: "0 0 1200 1200",
+    viewBox: "0 0 24 24",
     width: width ?? "24px",
     height: height ?? "24px",
     color: "color-mix(in oklab, hsl(0 0% 100%/1) 100%, #000 0%)"
--- src/VoiceHub/index.tsx
+++ src/VoiceHub/index.tsx
@@ -6,7 +6,7 @@
  */
 
 const { Patcher, Webpack, React, DOM, Data } = new BdApi('VoiceHub');
-const Module = Webpack.getBySource('ConnectedPrivateChannelsList');
+const Module = Webpack.getBySource('.Z.CONTACTS_LIST');
 
 const [VoiceIcon, ModalRoot, openModal, SearchIcon, VideoIcon, LiveStream] = BdApi.Webpack.getBulk(
     { filter: BdApi.Webpack.Filters.byStrings('"M15.16 16.51c-.57.28-1.16-.2-1.16-.83v-.14c0-.43.28-.8.63-1.02a3 3 0 0 0 0-5.04c-.35-.23-.63-.6-.63-1.02v-.14c0-.63.59-1.1 1.16-.83a5 5 0 0 1 0 9.02Z'), searchExports: true },
@@ -19,7 +19,7 @@ const [VoiceIcon, ModalRoot, openModal, SearchIcon, VideoIcon, LiveStream] = BdA
 
 const Eye = ({ width, height }) => (
     <svg
-        viewBox="0 0 1200 1200"
+        viewBox="0 0 24 24"
         width={width ?? '24px'}
         height={height ?? '24px'}
         color="color-mix(in oklab, hsl(0 0% 100%/1) 100%, #000 0%)"
