2c2cd5aadb888c8cc687119455e4c1c16b8be22a
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2025-08-28 19:24:25 -0500

    Wonky fix?

---
+++ src/MentionFix/index.tsx
@@ -0,0 +1,68 @@
+/**
+ * @name MentionFix
+ * @version 1.1.0
+ * @description Hate the `@unknown-user` when mentioning someone you've never met? Yeah this fixes that. :>
+ * @author Kaan
+ */
+const { Webpack, Patcher, ReactUtils } = new BdApi('MentionFix')
+const [Module, Key] = Webpack.getWithKey(Webpack.Filters.byStrings('viewingChannelId', 'parsedUserId'))
+const UserStore = Webpack.getStore('UserStore')
+const FetchModule = Webpack.getMangled('type:"USER_PROFILE_FETCH_START"', { fetchUser: Webpack.Filters.byStrings("USER_UPDATE", "Promise.resolve") })
+
+const Message = Webpack.getByKeys('interactionSending', 'quotedChatMessage').message
+
+function reRender(selector) {
+    const target = document.querySelector(selector)?.parentElement;
+    if (!target) return;
+    const instance = ReactUtils.getOwnerInstance(target);
+    const unpatch = Patcher.instead(instance, "render", () => unpatch());
+    instance.forceUpdate(() => instance.forceUpdate());
+}
+
+class MentionFix {
+    constructor() {
+        this.fetchedUsers = new Set()
+    }
+
+    start() {
+        Patcher.after(Module, Key, (that, [args], res) => {
+            const userId = args.parsedUserId
+            const doesUserExist = UserStore.getUser(userId)
+
+            if (doesUserExist === undefined) {
+                for (var child of res.props.children) {
+                    if (child && child.props) {
+                        const originalOnMouseEnter = child.props.onMouseEnter
+
+                        Object.defineProperty(child.props, 'onMouseEnter', {
+                            value: async (e) => {
+                                if (!this.fetchedUsers.has(userId)) {
+                                    await this.fetchedUsers.add(userId)
+                                    await FetchModule.fetchUser(userId).catch(error => {
+                                        this.fetchedUsers.delete(userId)
+                                    })
+                                }
+                                setTimeout(() => reRender(`.${Message}`), 10)
+
+                                if (originalOnMouseEnter) {
+                                    originalOnMouseEnter(e)
+                                }
+                            },
+                            writable: true,
+                            configurable: true
+                        })
+                    }
+                }
+            }
+            // setTimeout(() => reRender(`.${Message}`), 10)
+
+        })
+    }
+
+    stop() {
+        Patcher.unpatchAll()
+        this.fetchedUsers.clear()
+    }
+}
+
+module.exports = MentionFix;
