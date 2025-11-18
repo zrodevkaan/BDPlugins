57dae9309b23fdcca4c18669be395b41292acfce
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2025-11-18 11:17:01 -0600

    remove log

--- Plugins/owoify/owoify.plugin.js
+++ Plugins/owoify/owoify.plugin.js
@@ -69589,7 +69589,6 @@ var owoify = class {
         }
       ],
       execute: async (args, { channel }) => {
-        console.log(args);
         const data = await Net.fetch("https://anythingtranslate.com/wp-admin/admin-ajax.php", {
           "headers": {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0",
